f03c3b56be8d5110c47325447fffa909c9df881a
DESKTOP-KQA2RP4\ThinkPad <chinhpham1609@gmail.com>
2025-11-14 02:14:38 +0000

    fix trycatch

--- src/index.ts
+++ src/index.ts
@@ -29,69 +29,80 @@ app.use(morgan("dev"));
 
 
 (async () => {
-	const mongo = MongoConnection.getInstance()
-  	await mongo.connect()
+	try {
+		const mongo = MongoConnection.getInstance()
+		await mongo.connect()
 
-	const server = app.listen(PORT, () => {
-		logger.info(`Server is running at http://localhost:${PORT}`);
-	});
-
-	// pnpm run start:1
-	cleanupVisited(1);
-
-	const agentId = process.argv.find(arg => arg.startsWith('--id='))?.split('=')[1] || 'default';
-
-	const kafka = new Kafka({
-		clientId: `agent-${agentId}`,
-		brokers: ['103.97.125.64:9092'],
-		logLevel: logLevel.INFO
-	});
-	const consumer = kafka.consumer({ groupId: `web-group-${agentId}`, sessionTimeout: 600000 });
-
-	await consumer.connect();
-	await consumer.subscribe({ topic: 'unclassified_jobs_website', fromBeginning: false });
-
-	const { browser, page } = await initWeb(`agent-${process.pid}`);
-
-	await consumer.run({
-		autoCommit: false,
-		eachMessage: async ({ topic, partition, message, heartbeat }) => {
-			const raw = message.value?.toString()!;
-			const offset = message.offset;
-
-			const data = JSON.parse(raw);
-			const keyword = data.keyword;
-
-			try {
-				logger.info(`Agent ${process.pid} xử lý: "${keyword}" | partition: ${partition} | offset: ${message.offset}`);
-				await crawlerKafka(data, `agent-${process.pid}`, browser, page);
-				
-				await consumer.commitOffsets([{ topic, partition, offset: (Number(offset) + 1).toString() }]);
-				await heartbeat(); // giữ kết nối với Kafka
-				logger.info(`Đã commit xong tại offset ${Number(offset) + 1} trên phân vùng ${partition}`);
-				logger.info(`Agent ${agentId} đã khởi động và đang lắng nghe Kafka...`);
-			} catch (error: any) {
-				// logger.error(`Lỗi xử lý keyword "${keyword}": ${error.message}`);
-				logger.error(`Lỗi xử lý keyword "${keyword}"`);
-			}
-		}
-	});
-
-
-	const gracefulShutdown = async () => {
-		logger.info("Gracefully shutting down...");
-		await consumer.disconnect();
-		await mongo.disconnect();
-		server.close(() => {
-			logger.info("Server stopped, MongoDB connection closed");
-			process.exit(0);
+		const server = app.listen(PORT, () => {
+			logger.info(`Server is running at http://localhost:${PORT}`);
 		});
-	};
 
-	process.on("SIGINT", gracefulShutdown);
-	process.on("SIGTERM", gracefulShutdown);
+		// pnpm run start:1
+		cleanupVisited(1);
 
-	
+		const agentId = process.argv.find(arg => arg.startsWith('--id='))?.split('=')[1] || 'default';
+
+		const kafka = new Kafka({
+			clientId: `agent-${agentId}`,
+			brokers: ['103.97.125.64:9092'],
+			logLevel: logLevel.INFO
+		});
+		const consumer = kafka.consumer({ groupId: `web-group-${agentId}`, sessionTimeout: 600000 });
+
+		await consumer.connect();
+		await consumer.subscribe({ topic: 'unclassified_jobs_website', fromBeginning: false });
+
+		const { browser, page } = await initWeb(`agent-${process.pid}`);
+
+		await consumer.run({
+			autoCommit: false,
+			eachMessage: async ({ topic, partition, message, heartbeat }) => {
+				const raw = message.value?.toString()!;
+				const offset = message.offset;
+
+				// const data = JSON.parse(raw);
+				let data;
+				try {
+					data = JSON.parse(raw);
+				} catch (err: any) {
+					logger.error(`Lỗi parse JSON tại offset ${offset}: ${err.message}`);
+					return;
+				}
+
+				const keyword = data.keyword;
+
+				try {
+					logger.info(`Agent ${process.pid} xử lý: "${keyword}" | partition: ${partition} | offset: ${message.offset}`);
+					await crawlerKafka(data, `agent-${process.pid}`, browser, page);
+					
+					await consumer.commitOffsets([{ topic, partition, offset: (Number(offset) + 1).toString() }]);
+					await heartbeat(); // giữ kết nối với Kafka
+					logger.info(`Đã commit xong tại offset ${Number(offset) + 1} trên phân vùng ${partition}`);
+					logger.info(`Agent ${agentId} đã khởi động và đang lắng nghe Kafka...`);
+				} catch (error: any) {
+					// logger.error(`Lỗi xử lý keyword "${keyword}": ${error.message}`);
+					logger.error(`Lỗi xử lý keyword "${keyword}"`);
+				}
+			}
+		});
+
+
+		const gracefulShutdown = async () => {
+			logger.info("Gracefully shutting down...");
+			await consumer.disconnect();
+			await mongo.disconnect();
+			server.close(() => {
+				logger.info("Server stopped, MongoDB connection closed");
+				process.exit(0);
+			});
+		};
+
+		process.on("SIGINT", gracefulShutdown);
+		process.on("SIGTERM", gracefulShutdown);
+	} catch (error: any) {
+		logger.error("Lỗi khởi động ứng dụng:", error.message);
+		process.exit(1);
+	}
 
 	// const intervalMs = 15 * 1000;
 	// while (true) {
